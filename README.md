03732dd1971bd89d96ab7d244b4c493d51a9e470
jbobrutsy <jbobrutsky@gmail.com>
2022-03-11 11:38:39 +0200

    add latex equations

--- README.md
+++ README.md
@@ -5,3 +5,33 @@ Accuracy estimation using noisy sampling for geographical applications.
 $$X \sim \mathcal{N}(\mu, P) = \mathcal{N}(\mu, \overbrace{\theta P_0}^{P})\\
     Y \sim \mathcal{N}(\mu, G)\\
     Z = X-Y \sim \mathcal{N}(0, P+G)$$
+
+## Maximum Liklihood Estimator
+$$
+\mathcal{L}(\theta|z) = \frac{1}{2\pi\sqrt{\det(D)} }\exp{(-\frac{1}{2} z^T D^{-1} z)}
+$$
+
+when $D = P+G = \theta P_0 + G$.
+
+Allowing combining multilpe measurmetns with different variances:
+$$
+        \mathcal{L}(\theta|z_1, z_2, ..., z_n) = \prod_{i=1}^{n}{\frac{1}{2\pi\sqrt{\det(D_i)} }\exp{(-\frac{1}{2} z_i^T D_i^{-1} z_i)}}
+$$
+The log-liklihood would be:
+$$
+\ell(\theta|\{z_i\}_{i=1}^n) =   -n\log{2\pi} - \frac{1}{2}\sum_{i=1}^{n}{\log{(\det{D_i})}}  - \frac{1}{2}\sum_{i=1}^{n}{z_i^T D_i^{-1} z_i}
+$$
+
+Such that the MLE estimator is:
+$$
+\hat\theta = \argmin_{\theta}{\sum_{i=1}^{n}{\log{(\det{D_i})}  + z_i^T D_i^{-1} z_i}}
+$$
+
+In order to efficenly solve, we can calculate the gradient:
+$$
+-\nabla\ell(\theta|\{z_i\}_{i=1}^n) = \sum_{i=1}^{n}Tr(D_i^{-1}\frac{d D_i}{d\theta}) - z_i^T D_i^{-1} \frac{d D_i}{d\theta} D_i z_i
+$$
+and using $\frac{d D_i}{d\theta} = P_i$:
+$$
+-\nabla\ell(\theta|\{z_i\}_{i=1}^n) = \sum_{i=1}^{n}Tr(D_i^{-1}P_i) - z_i^T D_i^{-1} P_i D_i z_i
+$$
--- app.py
+++ app.py
@@ -14,23 +14,20 @@ st.latex(
     Z = X-Y \sim \mathcal{N}(0, P+G)"""
 )
 
-
 # parmeters
 col1, col2, col3 = st.columns(3)
 
-number_of_samples = 10 ** col1.slider("Num. of Samples: Order of Magnitude", 1, 4, 2)
+number_of_samples = 10 ** col1.slider("Num. of Samples: Order of Magnitude", 1, 5, 2)
 true_estimator_std = col2.slider("Estimator Standard Devietion", 1, 50, 10)
 measurment_std = col3.slider("Measurments Standard Devietion", 1, 10, 5)
 
 # generate samples
-with st.spinner("Wait for it..."):
-    estimtor_points = random.normal(
-        loc=0.0, scale=true_estimator_std, size=[number_of_samples, 2]
-    )
-    measurment_points = random.normal(
-        loc=0.0, scale=measurment_std, size=[number_of_samples, 2]
-    )
-st.success("Simulation is done!")
+def sample(number_of_samples=100, std=1.0):
+    return random.normal(loc=0.0, scale=std, size=[number_of_samples, 2])
+
+
+estimtor_points = sample(number_of_samples, true_estimator_std)
+measurment_points = sample(number_of_samples, measurment_std)
 
 difference_vectors = estimtor_points - measurment_points
 
@@ -38,9 +35,43 @@ difference_vectors = estimtor_points - measurment_points
 hist_data = [estimtor_points, measurment_points, difference_vectors]
 hist_data_norm = [np.linalg.norm(points, axis=1) for points in hist_data]
 
-
 group_labels = ["Estimators", "Measurments", "Differences"]
 
-fig = ff.create_distplot(hist_data_norm, group_labels)  # create plotly object
+with st.spinner("Wait for it..."):
+    fig = ff.create_distplot(hist_data_norm, group_labels)  # creates plotly object
+    st.plotly_chart(fig, use_container_width=True)
 
-st.plotly_chart(fig, use_container_width=True)
+st.header("Maximum Liklihood Estimator")
+
+st.markdown(
+    r"""
+$$
+\mathcal{L}(\theta|z) = \frac{1}{2\pi\sqrt{\det(D)} }\exp{(-\frac{1}{2} z^T D^{-1} z)}
+$$
+
+when $D = P+G = \theta P_0 + G$.
+
+Allowing combining multilpe measurmetns with different variances:
+$$
+        \mathcal{L}(\theta|z_1, z_2, ..., z_n) = \prod_{i=1}^{n}{\frac{1}{2\pi\sqrt{\det(D_i)} }\exp{(-\frac{1}{2} z_i^T D_i^{-1} z_i)}}
+$$
+The log-liklihood would be:
+$$
+\ell(\theta|\{z_i\}_{i=1}^n) =   -n\log{2\pi} - \frac{1}{2}\sum_{i=1}^{n}{\log{(\det{D_i})}}  - \frac{1}{2}\sum_{i=1}^{n}{z_i^T D_i^{-1} z_i}
+$$
+
+Such that the MLE estimator is:
+$$
+\hat\theta = \argmin_{\theta}{\sum_{i=1}^{n}{\log{(\det{D_i})}  + z_i^T D_i^{-1} z_i}}
+$$
+
+In order to efficenly solve, we can calculate the gradient:
+$$
+-\nabla\ell(\theta|\{z_i\}_{i=1}^n) = \sum_{i=1}^{n}Tr(D_i^{-1}\frac{d D_i}{d\theta}) - z_i^T D_i^{-1} \frac{d D_i}{d\theta} D_i z_i
+$$
+and using $\frac{d D_i}{d\theta} = P_i$:
+$$
+-\nabla\ell(\theta|\{z_i\}_{i=1}^n) = \sum_{i=1}^{n}Tr(D_i^{-1}P_i) - z_i^T D_i^{-1} P_i D_i z_i
+$$
+"""
+)
